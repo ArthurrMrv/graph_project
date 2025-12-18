3977185a6fb41afe42c1bd9c468ff492291942ab
Othmane Menkor <othmane.menkor@gmail.com>
2025-12-18 21:48:06 +0100

    New Data ingestion pipeline with relation between trading day and tweet day + augmented stock information

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routers import sentiment, analytics, ingestion, quantitative
+from app.routers import sentiment, analytics, ingestion, quantitative, pipeline
 
 app = FastAPI(title="Stock Sentiment Graph API")
 
 app.include_router(ingestion.router, prefix="/api", tags=["Ingestion"])
 app.include_router(sentiment.router, prefix="/api/sentiment", tags=["Sentiment"])
 app.include_router(analytics.router, prefix="/api", tags=["Analytics"])
+app.include_router(pipeline.router, prefix="/api/pipeline", tags=["Pipeline"])
 app.include_router(quantitative.router, prefix="/api", tags=["Quantitative Analysis"])
 
 @app.get("/")
